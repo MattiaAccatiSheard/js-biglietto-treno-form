804ab93a3a6ba24402bafb90f41d7362481e903b
MattiaAccatiSheard <mattiahtsheard@gmail.com>
2023-01-27 17:44:14 +0100

    .finished

--- Js/main.js
+++ Js/main.js
@@ -1,45 +1,40 @@
-let element = document.getElementById("hey");
-element.style.color= "red"
-element.addEventListener('click', function () {
-    alert("Il bottone è stato cliccato!");
-});
+const generator = document.getElementById('ticket-generator');
 
-let buttonCalculateEl = document.getElementById("ticket-generator");
-let inputAgeEl = document.getElementById("age");
-let inputKmEl = document.getElementById("km");
+generator.addEventListener(
+    'click',
 
+    function () {
 
-buttonCalculateEl.addEventListener('click', function(){
-    console.log('l\'età dell\'utente è:', inputAgeEl.value);
-    console.log('l\'utente percorrerà', inputKmEl.value + 'km');
-    
-    //chiedo età
-    let age = parseInt(prompt('quanti anni hai?'))
-    //chitedo lunghezza del viaggio
-    let kilometri = parseFloat(prompt('quanti kilometri viaggerai?'))
-
-    //prova console
-    console.log('L\'utente ha' + ' ' + age + ' ' + 'anni e viaggerà per' + ' ' + kilometri + 'km');
+        const Km = parseInt(document.getElementById('km').value);
+        const age = document.getElementById('age').value;
+        const allPrice = Km * 0.21;
+        let price;
 
 
-    //calcolo del prezzo
-    let price = parseFloat(0.21 * kilometri).toFixed(2);
-    console.log('il prezzo del biglietto è:' + ' ' + price + '$');
+        if (!isNaN(Km)) {
 
-    //applico gli sconti
-    if (age <= 17) {
-        price = (price - (price / 100 * 20)).toFixed(2);
-        console.log(', ma grazie allo sconto del 20% il minorenne pagherà:' + price + '$');
-    } else if (age >= 65) {
-        price = (price - (price / 100 * 40)).toFixed(2);
-        console.log(', ma grazie allo sconto del 40% l\'anziano pagherà:' + price + '$');
-    }
-    else (18 <= age <= 65); {
-        price = price;
-    }
-    document.getElementById("ticket-result").innerHTML=price;
-});
+            if (age < 18) {
 
+                price = (allPrice * .2);
 
+            } else if (age >= 65) {
 
+                price = (allPrice * .4);
 
+            } else {
+
+                price = allPrice;
+
+            }
+
+        } else {
+            alert('valore dei km non valido: inserire un numero')
+        }
+
+        
+        price = price.toFixed(2);
+
+        //output
+        document.getElementById('ticket-result').innerHTML = `Costo biglietto: ${price} euro`;
+
+    })
